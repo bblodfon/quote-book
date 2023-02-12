8bdb1fbc412497bdd3b37fcf2ed9849a67111903
john <bblodfon@gmail.com>
2023-02-12 19:31:24 +0100

    set path of pandoc (in case it is needed)

--- build_book.sh
+++ build_book.sh
@@ -1,3 +1,6 @@
-Rscript -e "bookdown::render_book(input = 'index.Rmd', output_format = 'bookdown::gitbook')"
+Rscript -e "
+Sys.setenv(RSTUDIO_PANDOC = '/usr/lib/rstudio/resources/app/bin/quarto/bin/tools'); 
+bookdown::render_book(input = 'index.Rmd', output_format = 'bookdown::gitbook')
+"
 xdg-open docs/index.html
 
